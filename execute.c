faac5310eac474f6faf826cd5e7ecc28de54d067
Zamoo6 <a.ata.e.001@gmail.com>
2023-11-06 11:39:04 -0800

    update execute file

--- execute.c
+++ execute.c
@@ -1,6 +1,6 @@
 #include "main.h"
 #include <sys/wait.h>
-/*
+/**
  * _execute - function to exectue command's
  * Return: wating for now command
  */
@@ -8,16 +8,28 @@
 void exec_com(const char *com)
 {
 	pid_t child_pid = fork();
-
+	
 	if (child_pid == -1)
 	{
-		perror("fork");
+		_printf("Warrning: Error process forking");
 		exit(EXIT_FAILURE);
 	}
 	else if (child_pid ==0)
 	{
-		execlp(com, com, (char *)NULL);
-		perror("execlp");
+		char *args[256];
+		int arg_count = 0;
+		
+		char *token = strtok((char *)com, " ");
+		while (token != NULL)
+		{
+			args[arg_count++] = token;
+			token = strtok(NULL, " ");
+		}
+		args[arg_count] = NULL;
+
+		execvp(args[0], args);
+
+		_printf("Warrning: Error comand executing.\n");
 		exit(EXIT_FAILURE);
 	}
 	else
