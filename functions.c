1d2dbf622a94585ac3f9f4227316c7bbe92c814d
Abeermohsien <aboorakhalil993@gmail.com>
2023-11-06 13:42:09 -0800

    $

---
+++ functions.c
@@ -0,0 +1,23 @@
+# include "main.h"
+char *_strdup(const char *str)
+{
+char *ptr;
+int i, len = 0;
+if (str == NULL)
+	return (NULL);
+while (*str != '\0')
+{
+len++;
+str++;
+}
+str = str - len;
+ptr = malloc(sizeof(char) * (len + 1));
+if (ptr == NULL)
+	return (NULL);
+for (i = 0; i <= len; i++)
+ptr[i] = str[i];
+return (ptr);
+}
+int _strcmp(char *s1, char *s2)
+{
+
--- main.h
+++ main.h
@@ -7,6 +7,11 @@
 #include <stdlib.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+char *_strdup(const char *str);
+int _strcmp(char *s1, char *s2);
+int _strlen(char *s);
+char *_strcat(char *dest, char *src);
+char *_strcpy(char *dest, char *src);
 void execthree(char **argv);
 void print_prompt(void);
 void _printf(const char *string);
