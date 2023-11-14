31687fc994ad45955b816dca6d04b315745444e5
Abeermohsien <aboorakhalil993@gmail.com>
2023-11-14 01:59:04 -0800

    $

--- functions.c
+++ functions.c
@@ -1,4 +1,4 @@
-# include "main.h"
+#include "main.h"
 /**
  * _strdup - string duplecation
  * @str: string
--- main.c
+++ main.c
@@ -1,19 +1,27 @@
 #include "main.h"
-/*
- * _main - privet program emulate CMD
+
+/**                                              * main - check cide
+ * @ac: args num
+ * @argv: args
  * Return: 0
- */
-
-int main(void)
+ */                                             int main(int ac, char **argv)
 {
-	char com[64];
-
-	while (1)
-	{
-		print_prompt();
-		read_com(com, sizeof(com));
-		exec_com(com);
-	}
-
-	return 0;
+char *the_line = NULL;
+char **command = NULL;
+int status = 0, idx = 0;
+(void)ac;
+while (1)
+{                                               the_line = _read();
+if (the_line == NULL)
+{
+if (isatty(STDIN_FILENO))
+        write(STDOUT_FILENO, "\n", 1);
+return (status);
+}
+idx++;
+command = _token(the_line);
+if (!command)
+        continue;
+status = _execution(command, argv, idx);
+}
 }
