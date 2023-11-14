38819116436c1d46a1a7a53498a2e5ac59c26ca1
Zamoo6 <a.ata.e.001@gmail.com>
2023-11-14 07:51:42 -0800

    @ad

--- execute1.c
+++ execute1.c
@@ -3,35 +3,39 @@
  * _execute - execute the program
  * @command: str
  * @argv: str
+ * @idx: int parameter
  * Return: in
  */
+
 int _execute(char **command, char **argv, int idx)
 {
-char *full;
-pid_t child;
-int status;
-full = _getpath(command[0]);
-if (!full)
-{
-print_error(argv[0], command[0], int idx);
-freearr(command);
-return (127);
-}
-child = fork();
-if (child == 0)
-{
-if (execve(full, command, environ) == -1)
-{
-free (full);
-full = NULL;
-freearr(command);
-}
-}
-else
-{
-waitpid(child, &status, 0);
-freearr(command);
-free(full), full = NULL;
-}
-return (WEXITSTATUS(status));
+	char *full;
+	pid_t child;
+	int status;
+
+	full = _getpath(command[0]);
+
+	if (!full)
+	{
+		print_error(argv[0], command[0], int idx);
+		freearr(command);
+		return (127);
+	}
+	child = fork();
+	if (child == 0)
+	{
+		if (execve(full, command, environ) == -1)
+		{
+			free(full);
+			full = NULL;
+			freearr(command);
+		}
+	}
+	else
+	{
+		waitpid(child, &status, 0);
+		freearr(command);
+		free(full), full = NULL;
+	}
+	return (WEXITSTATUS(status));
 }
--- functions.c
+++ functions.c
@@ -4,91 +4,100 @@
  * @str: string
  * Return: string
  */
+
 char *_strdup(const char *str)
 {
-char *ptr;
-int i, len = 0;
-if (str == NULL)
-	return (NULL);
-while (*str != '\0')
-{
-len++;
-str++;
+	char *ptr;
+	int i, len = 0;
+	if (str == NULL)
+		return (NULL);
+	while (*str != '\0')
+	{
+		len++;
+		str++;
+	}
+	str = str - len;
+	ptr = malloc(sizeof(char) * (len + 1));
+	if (ptr == NULL)
+		return (NULL);
+	for (i = 0; i <= len; i++)
+		ptr[i] = str[i];
+	return (ptr);
 }
-str = str - len;
-ptr = malloc(sizeof(char) * (len + 1));
-if (ptr == NULL)
-	return (NULL);
-for (i = 0; i <= len; i++)
-ptr[i] = str[i];
-return (ptr);
-}
+
 /**
  * _strcmp - string compare
  * @s1: string
  * @s2: string
  * Return: int
  */
+
 int _strcmp(char *s1, char *s2)
 {
-int cmp;
-cmp = (int)*s1 - (int)*s2;
-while (*s1)
-{
-if (*s1 != *s2)
-	break;
-s1++;
-s2++;
-cmp = (int)*s1 - (int)*s2;
+	int cmp;
+	cmp = (int)*s1 - (int)*s2;
+	while (*s1)
+	{
+		if (*s1 != *s2)
+			break;
+		s1++;
+		s2++;
+		cmp = (int)*s1 - (int)*s2;
+	}
+	return (cmp);
 }
-return (cmp);
-}
+
 /**
  * _strlen - count string length
  * @s: string
  * Return: int
  */
+
 int _strlen(char *s)
 {
-int n = 0;
-while (s[len])
-	len++;
-return (len);
+	int n = 0;
+	while (s[len])
+		len++;
+	return (len);
 }
+
 /**
  * _strcat - str cut
  * @d: str
  * @src: str
  * Return: str
  */
+
 char *_strcat(char *d, char *src)
 {
-char *p = d;
-while (*p)
-	p++;
-while(*src)
-{
-*p = *src;
-p++;
-src++;
+	char *p = d;
+	while (*p)
+		p++;
+	while (*src)
+	{
+		*p = *src;
+		p++;
+		src++;
+	}
+	*p = '\0';
+	return (d);
 }
-*p = '\0';
-return (d);
-}
+
 /**
  * _strcpy - str copy
  * @d: str
  * @src: str
  * Return: srr
  */
+
 char *_strcpy(char *d, char *src)
 {
-int i = 0;
-while (src[i])
-{
-d[i] = src[i];
-i++;
-}
-d[i] = '\0';
-return (d);
+	int i = 0;
+	while (src[i])
+	{
+		d[i] = src[i];
+		i++;
+	}
+	d[i] = '\0';
+	return (d);
 }
