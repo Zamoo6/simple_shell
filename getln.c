c8e0c6f870ae4872234b5ce695c939f62c5ab02b
Abeermohsien <aboorakhalil993@gmail.com>
2023-11-16 05:11:25 -0800

    $

--- getln.c
+++ getln.c
@@ -1,36 +1,41 @@
 #include "shell.h"
-/**                                              * _hotline_STDIN - gets the next line of input
-from STDIN                                       * @info: parameter struct                       * @ptr: address of pointer to buffer, preallocated or NULL
- * @length: size of preallocated ptr buffer if n
-ot NULL
- *
+/**                                              * _hotline_STDIN - gets the next lin
+ * @info: char
+ * @ptr: char
+ * @length: int
  * Return: s
  */                                             int _hotline_STDIN(info_t *info, char **ptr, size_t *length)                                    {
-        static char buf[READ_BUF_SIZE];
-        static size_t i, len;
-        size_t k;
-        ssize_t r = 0, s = 0;
-        char *p = NULL, *new_p = NULL, *c;
-
-	p = *ptr;
-	if (p && length)
+	 static char buf[READ_BUF_SIZE];
+	 static size_t i, len;
+	 size_t k;
+	 ssize_t r = 0, s = 0;
+	 char *p = NULL, *new_p = NULL, *c;
+	 
+	 p = *ptr;
+	 if (p && length)
 		s = *length;
-	if (i == len)
+	 if (i == len)
 		i = len = 0;
-
-	r = read_buf(info, buf, &len);
-        if (r == -1 || (r == 0 && len == 0))                    return (-1);
-	c = _strchr(buf + i, '\n');                     k = c ? 1 + (unsigned int)(c - buf) : len;
-        new_p = _realloc(p, s, s ? s + k : k + 1);
-        if (!new_p) /* MALLOC FAILURE! */                       return (p ? free(p), -1 : -1);
-
-	if (s)
-		_strncat(new_p, buf + i, k - i);
-	else
-                _strncpy(new_p, buf + i, k - i + 1);
-
+	 
+	 r = read_buf(info, buf, &len);
+	 if (r == -1 || (r == 0 && len == 0))                    return (-1);
+	 c = _strchr(buf + i, '\n');                     k = c ? 1 + (unsigned int)(c - buf) : len;
+	 new_p = _realloc(p, s, s ? s + k : k + 1);
+	 if (!new_p) /* MALLOC FAILURE! */                       return (p ? free(p), -1 : -1);
+	 if (s)
+		 _strncat(new_p, buf + i, k - i)
+	 else
+		 _strncpy(new_p, buf + i, k - i + 1);
 	s += k - i;
-        i = k;                                          p = new_p;                                                                                      if (length)                                             *length = s;                            *ptr = p;                                       return (s);                             }
+	i = k;
+	p = new_p;
+	
+	
+	if (length)
+		*length = s;
+	*ptr = p;
+	return (s);
+}
 /**
  * chc_input_buf - buffers chained commands
  * @info: parameter struct
@@ -43,10 +48,9 @@ ssize_t chc_input_buf(info_t *info, char **buf, size_t *len)
 {
 	ssize_t r = 0;
 	size_t len_p = 0;
-
+	
 	if (!*len)
 	{
-		
 		free(*buf);
 		*buf = NULL;
 		signal(SIGINT, s_Handler);
