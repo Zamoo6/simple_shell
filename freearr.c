aaba0edb41a998eefd8a97396751bedfdc0950b5
Zamoo6 <a.ata.e.001@gmail.com>
2023-11-16 01:29:57 -0800

    ad

--- freearr.c
+++ freearr.c
@@ -1,16 +1,29 @@
 #include "shell.h"
 /**
- * _strcat - concatenates two strings            * @dest: the destination buffer                 * @src: the source buffer                       *
+ * _strcat - concatenates two strings
+ * * @dest: the destination buffer
+ * * @src: the source buffer
+ * *
  * Return: pointer to destination buffer
- */                                             char *_strcat(char *dest, char *src)            {                                                       char *ret = dest;
-                                                        while (*dest)                                           dest++;                                 while (*src)                                            *dest++ = *src++;
-        *dest = *src;                                   return (ret);                           }
+ */
+char *_strcat(char *dest, char *src)
+{
+	char *ret = dest;
+	while (*dest)
+		dest++;
+	while (*src)
+		*dest++ = *src++;
+	*dest = *src;
+	return (ret);
+}
+
 /**
  * _strlen - returns the length of a string
  * @s: the string whose length to check
  *
  * Return: integer length of string
  */
+
 int _strlen(char *s)
 {
 	int i = 0;
--- tokenizer.c
+++ tokenizer.c
@@ -21,9 +21,14 @@ char *_strdup(const char *str)
 		ret[length] = *--str;
 	return (ret);
 }
-/**                                              * _strcpy - copies a string
- * @dest: the destination                        * @src: the source
- *                                               * Return: pointer to destination                */
+
+/**
+ * _strcpy - copies a string
+ * @dest: the destination
+ * @src: the source
+ * Return: pointer to destination
+ */
+
 char *_strcpy(char *dest, char *src)            {
         int i = 0;
 
@@ -35,15 +40,28 @@ char *_strcpy(char *dest, char *src)            {
         dest[i] = 0;
         return (dest);
 }
-/**                                              * _strlen - returns the length of a string      * @s: the string whose length to check          *                                               * Return: integer length of string              */                                             int _strlen(char *s)                            {                                                       int i = 0;                                                                                      if (!s)                                                 return (0);                                                                             while (*s++)                                            i++;
-        return (i);
+/**
+ * _strlen - returns the length of a string
+ * @s: the string whose length to check
+ * Return: integer length of string
+ * */
+int _strlen(char *s)
+{
+	int i = 0;
+	if (!s)
+		return(0);
+	while (*s++)
+		i++;
+	return(i);
 }
+
 /**
  *_puts - prints an input string
  i*@str: the string to be printed
  *
  * Return: Nothing
  */
+
 void _puts(char *str)
 {
 	int i = 0;
